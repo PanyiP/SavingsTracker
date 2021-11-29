05db3c3be077dee72587289e0c2200c5996fb15e
Peter Panyi <peterpanyi85@gmail.com>
2021-11-29 20:28:48 +0100

    Initial VS solution

---
+++ SavingsTracker/SavingsTracker/SavingsTracker/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SavingsTracker.Services;
+using SavingsTracker.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SavingsTracker
+{
+   public partial class App : Application
+   {
+
+      public App()
+      {
+         InitializeComponent();
+
+         DependencyService.Register<MockDataStore>();
+         MainPage = new AppShell();
+      }
+
+      protected override void OnStart()
+      {
+      }
+
+      protected override void OnSleep()
+      {
+      }
+
+      protected override void OnResume()
+      {
+      }
+   }
+}
---
+++ SavingsTracker/SavingsTracker/SavingsTracker/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using SavingsTracker.ViewModels;
+using SavingsTracker.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SavingsTracker
+{
+   public partial class AppShell : Xamarin.Forms.Shell
+   {
+      public AppShell()
+      {
+         InitializeComponent();
+         Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+         Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+      }
+
+      private async void OnMenuItemClicked(object sender, EventArgs e)
+      {
+         await Shell.Current.GoToAsync("//LoginPage");
+      }
+   }
+}
